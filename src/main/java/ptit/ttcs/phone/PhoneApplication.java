3e87684f54c2d532d01bd5b9f3248c679dae4e6a
Dung Dinh <100460297+dungprolhvn@users.noreply.github.com>
2026-03-30 22:01:56 +0700

    first commit

---
+++ src/main/java/ptit/ttcs/phone/PhoneApplication.java
@@ -0,0 +1,13 @@
+package ptit.ttcs.phone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneApplication.class, args);
+	}
+
+}
