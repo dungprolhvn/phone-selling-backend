b0fd8a2b1ea5fbed5cfb00369778b9f55538aef3
Le Dang Ninh <dangninh010@gmail.com>
2026-04-05 23:19:06 +0700

    Add homepage APIs and caching

--- src/main/java/ptit/ttcs/phone/config/SecurityConfig.java
+++ src/main/java/ptit/ttcs/phone/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package ptit.ttcs.phone.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,6 +11,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 import ptit.ttcs.phone.security.JwtAuthFilter;
 
 @Configuration
@@ -28,7 +29,8 @@ public class SecurityConfig {
             .requestMatchers(
                 "/api/auth/**",
                 "/api/products/**",
-                "/api/brands/**",
+              "/api/brands",
+              "/api/brands/**",
                 "/api/cart/**",
                 "/api/orders/status",
                 // api docs
